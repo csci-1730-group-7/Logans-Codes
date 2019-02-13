548f8e81b562625ee39d602e971ebac1f1a5768d
SrSabetodo <lkreun@oaala.com>
2019-02-13 10:56:42 -0600

    Create Rock_Paper_Scissors.cpp

---
+++ Rock_Paper_Scissors.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int getCompSelection();
+int getUserSelection();
+int playResults(int userSelect, int compSelect);
+
+int main()
+{
+srand (time(NULL));
+
+int userSelect;
+int compSelect;
+int winOrLoss;
+int compWins=0;
+int userWins=0;
+char playAgain;
+
+do
+{
+cout<<"It's time to play Rock, Paper, Scissors!\n";
+userSelect=getUserSelection();
+compSelect=getCompSelection();
+
+winOrLoss=playResults(userSelect,compSelect);
+
+if(winOrLoss==1){userWins++;}
+else if(winOrLoss==-1){compWins++;}
+
+cout<<"Continue? (y/n)\n";
+cin>>playAgain;
+
+}while(playAgain=='y');
+
+if(playAgain=='n')
+	{
+	cout<<"I won "<<compWins<<" time(s)"<<endl<<"You won "<<userWins<<" time(s)";
+	}
+return 0;
+}
+
+int getCompSelection()
+{
+int computerMove;
+computerMove=rand()%3+1;
+return computerMove;
+}
+
+int getUserSelection()
+{
+int selection;
+int userWin;
+int compWin;
+
+cout<<"What is your play?\n";
+cout<<"Rock (1), Paper(2), Scissors (3)\n";
+cin>>selection;
+if(selection==1){return 1;}
+else if(selection==2){return 2;}
+else if(selection==3){return 3;}
+else{cout<<"Please enter a valid move you stupid human\n";}
+}
+
+int playResults(int userSelect, int compSelect)
+{
+if(userSelect==compSelect){cout<<"It was a tie\n";}
+
+if(compSelect==1)
+{
+	if(userSelect==2){cout<<"You played paper and I played rock"<<endl<<"You win!"<<endl;return 1;}
+	else if(userSelect==3){cout<<"You played paper and I played rock"<<endl<<"I win!"<<endl;return -1;}
+}
+if(compSelect==2)
+{
+	if(userSelect==1){cout<<"You played rock and I played paper"<<endl<<"I win!"<<endl;return -1;}
+	else if(userSelect==3){cout<<"You played rock and I played scissors"<<endl<<"You win!"<<endl;return 1;}
+}
+if(compSelect==3)
+{
+	if(userSelect==1){cout<<"You played rock and I played scissors"<<endl<<"You win!"<<endl;return 1;}
+	else if(userSelect==2){cout<<"You played paper and I played scissors"<<endl<<"I win!"<<endl;return -1;}
+}
+}
