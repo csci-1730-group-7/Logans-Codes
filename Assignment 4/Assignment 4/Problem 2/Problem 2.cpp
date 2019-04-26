8fb0d387d9e8c611bd0c0c8d4c481e8d345c31cc
SrSabetodo <47395031+SrSabetodo@users.noreply.github.com>
2019-04-26 16:43:53 -0500

    Here we go again...

--- Assignment 4/Assignment 4/Problem 2/Problem 2.cpp
+++ Assignment 4/Assignment 4/Problem 2/Problem 2.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 
 
-void cActions( );
-void vActions( );
+void cActions();
+void vActions();
 
 class Pairs
 {
 public:
-	Pairs( );
+	Pairs();
 	Pairs(Pairs n, Pairs i);
 	//friend operator+ (Pairs n);
 	//friend operator- (Pairs n);
 	//friend operator== (Pairs n, Pairs i);
 
 private:
-	float a,b,c;
+	float a, b, c;
 };
 //___________________________________________________________________
-class Complex: public Pairs
+class Complex : public Pairs
 {
-	public:
+public:
 	//default constructor
 	Complex();
 	Complex(double a, double b);
@@ -32,10 +32,10 @@ class Complex: public Pairs
 	//friend istream &operator >> (istream &is, Complex &number);
 
 	//simple arthimatic functions
-	const Complex operator+(const Complex &numberOne);
-	const Complex operator-(const Complex &numberOne);
-	const Complex operator*(const Complex &numberOne);
-	const Complex operator/(const Complex &numberOne);
+	const Complex operator+(const Complex& numberOne);
+	const Complex operator-(const Complex& numberOne);
+	const Complex operator*(const Complex& numberOne);
+	const Complex operator/(const Complex& numberOne);
 
 	//not so simple mierda 
 	Complex checkEquality();
@@ -52,96 +52,96 @@ private:
 
 };
 //____________________________________________________________________________
-class Vect: public Pairs
+class Vect : public Pairs
 {
 	//include overloaded multiplication and division
 	//friend insertaion and extraction operators 
-	public:
-	friend ostream &operator << (ostream &os, const Vect &number);
-	friend istream &operator >> (istream &is, Vect &number);
-	
+public:
+	friend ostream& operator << (ostream& os, const Vect& number);
+	friend istream& operator >> (istream& is, Vect& number);
+
 	const int operator* (Vect n);//dot product 
 	const int operator* (float n);//scalar multiplication 
-	
+
 	//include two overloaded multiplication operators 
 	//friend input and output stuff
-	
-	private:
-	float a,b;
+
+private:
+	float a, b;
 };
 //______________________________________________________________________________
-Pairs::Pairs( ){a=0;b=0;}
-Pairs::Pairs(Pairs n, Pairs i){a=n.a;b=i.a;}
+Pairs::Pairs() { a = 0; b = 0; }
+Pairs::Pairs(Pairs n, Pairs i) { a = n.a; b = i.a; }
 
 int main()
 {
 	int sel;
-	do{
+	do {
 		cout << "Select an option - (1) perform complex number actions\n";
 		cout << "                   " << "(2) perform vector actions\n";
 		cout << "                   " << "(3) exit\n";
-		cin>>sel;
-		if(sel==1)
-		  cActions( );
-		else if(sel==2)
-		  vActions( );
-		else if(sel==3)
-		  cout << "Bye...\n";
+		cin >> sel;
+		if (sel == 1)
+			cActions();
+		else if (sel == 2)
+			vActions();
+		else if (sel == 3)
+			cout << "Bye...\n";
 		else
-		  cout << "Invalid input - try again\n";
-	}while(sel != 3);
-	char ch; cin>>ch;
+			cout << "Invalid input - try again\n";
+	} while (sel != 3);
+	char ch; cin >> ch;
 	return 0;
 }
 
-void cActions( )
+void cActions()
 {
 	int resp;
-	switch(resp){
-		case 1: 
+	switch (resp) {
+	case 1:
 		cout << "Enter a complex number: ";
 		//
 		break;
-		case 2:
+	case 2:
 		cout << "Display a complex number: ";
 		//
 		break;
-		case 3:
+	case 3:
 		cout << "Perform arithmatic or equality comparisons of complex numbers: ";
 		//
 		break;
-		case 4: 
-		cout << "Bye...\n";break;
+	case 4:
+		cout << "Bye...\n"; break;
 		break;
 	}
 }
 
-void vActions( )
+void vActions()
 {
-		int resp;
-	switch(resp){
-		case 1: 
+	int resp;
+	switch (resp) {
+	case 1:
 		cout << "Enter a vector: ";
 		//
 		break;
-		case 2:
+	case 2:
 		cout << "Display a vector: ";
 		//
 		break;
-		case 3:
+	case 3:
 		cout << "Display a scalar: ";
 		//
 		break;
-		case 4:
+	case 4:
 		cout << "Perform arithmatic or equality comparisons of vectors: ";
 		//
 		break;
-		case 5: 
-		cout << "Bye...\n";break;
+	case 5:
+		cout << "Bye...\n"; break;
 		break;
 	}
 }
- 
+
 
 
 //placeholder
